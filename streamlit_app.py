c23bed232bf1dfe88a74b0cd1955210d7f1f90a9
EthanDem <113375436+EthanDem@users.noreply.github.com>
2023-09-18 10:49:07 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,40 +25,34 @@ st.title("Prompt Builder GUI")
 
 original_text = '''[EXPERT DETAILS] You have been hired by [COMPANY/PERSON NAME] to [NEEDED OUTPUT]
 [POINT OF VIEW] [GOAL]
-In order to provide a perfect and complete output, access any and all information you have access,
-which includes: files and content stored in your database, attachments provided if any,
-as well as any content provided within this prompt. Your output needs to be complete and final
-when you provide the output as this will be sent directly to a user without me reviewing it
-so it should always be a completed output. Do not use inert tags or any placeholder text.
-If you feel there should be an area that requires a custom input or placeholder text,
-please write around the need. This needs to be an output ready for a user to see.
-Strictly adhere to the WRITING STYLE provided for tone, overall style of the content
-and capture the voice as much as you can.
-Please follow the following formatting requirements:
-2.) MARKUP - Please use the following Markup and Structure details for the output provided here: [MARKUP]
-3.) WRITING STYLE= [WRITING STYLE]
+...'''
 
-Ok, that is the base instructions, now follow these details and create an output described provided here:
-USER PROMPT= '''
+company_names = []
+expert_details_list = []
+needed_outputs = []
+writing_styles = []
+points_of_view = []
+goals = []
+markups = []
 
 for i in range(num_instances):
     with st.expander(f'Instance {i + 1}', expanded=True):
-        company_name = st.text_input(f"Company/Person Name {i + 1}")
-        expert_details = st.text_input(f"Expert Details {i + 1}", value=expert_details_map.get(selected_expert, ""))
-        needed_output = st.text_input(f"Needed Output {i + 1}", value=needed_output_map.get(selected_output, ""))
-        writing_style = st.text_input(f"Writing Style {i + 1}", value=writing_style_map.get(selected_style, ""))
-        point_of_view = st.text_input(f"Point of View {i + 1}", value=point_of_view_map.get(selected_pov, ""))
-        goal = st.text_input(f"Goal {i + 1}", value=goal_map.get(selected_goal, ""))
-        markup = st.text_input(f"Markup {i + 1}", value=markup_map.get(selected_markup, ""))
+        company_names.append(st.text_input(f"Company/Person Name {i + 1}"))
+        expert_details_list.append(st.text_input(f"Expert Details {i + 1}", value=expert_details_map.get(selected_expert, "")))
+        needed_outputs.append(st.text_input(f"Needed Output {i + 1}", value=needed_output_map.get(selected_output, "")))
+        writing_styles.append(st.text_input(f"Writing Style {i + 1}", value=writing_style_map.get(selected_style, "")))
+        points_of_view.append(st.text_input(f"Point of View {i + 1}", value=point_of_view_map.get(selected_pov, "")))
+        goals.append(st.text_input(f"Goal {i + 1}", value=goal_map.get(selected_goal, "")))
+        markups.append(st.text_input(f"Markup {i + 1}", value=markup_map.get(selected_markup, "")))
 
 if st.button("Generate Prompt"):
     for i in range(num_instances):
-        updated_text = original_text.replace("[COMPANY/PERSON NAME]", company_name)
-        updated_text = updated_text.replace("[EXPERT DETAILS]", expert_details)
-        updated_text = updated_text.replace("[NEEDED OUTPUT]", needed_output)
-        updated_text = updated_text.replace("[WRITING STYLE]", writing_style)
-        updated_text = updated_text.replace("[POINT OF VIEW]", point_of_view)
-        updated_text = updated_text.replace("[GOAL]", goal)
-        updated_text = updated_text.replace("[MARKUP]", markup)
+        updated_text = original_text.replace("[COMPANY/PERSON NAME]", company_names[i])
+        updated_text = updated_text.replace("[EXPERT DETAILS]", expert_details_list[i])
+        updated_text = updated_text.replace("[NEEDED OUTPUT]", needed_outputs[i])
+        updated_text = updated_text.replace("[WRITING STYLE]", writing_styles[i])
+        updated_text = updated_text.replace("[POINT OF VIEW]", points_of_view[i])
+        updated_text = updated_text.replace("[GOAL]", goals[i])
+        updated_text = updated_text.replace("[MARKUP]", markups[i])
         st.subheader(f"Prompt for Instance {i + 1}")
         st.write(updated_text)
