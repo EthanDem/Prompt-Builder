922a2e4d6c3a2e5bf4e591829a0da0213a9d2a54
EthanDem <113375436+EthanDem@users.noreply.github.com>
2023-09-19 09:51:16 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import json
 
 # Load templates from JSON file
-with open('/mount/src/prompt-builder/updated_prompt_builder_templates.json', 'r') as f:
+with open('/mount/src/prompt-builder/final_updated_prompt_builder_templates.json', 'r') as f:
     templates = json.load(f)
 
 # Create maps for each template category
