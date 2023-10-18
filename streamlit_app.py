42a8a85043d04e8858626e777303ae2c71418ee1
EthanDem <113375436+EthanDem@users.noreply.github.com>
2023-10-18 15:33:30 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,71 +6,61 @@ with open('/app/prompt-builder/fixed_fully_updated_prompt_builder_templates.json
     templates = json.load(f)
 
 # Create maps for each template category
-act_as_map = {item['Name']: item['Prompt'] for item in templates.get('Act as if you are a...', [])}
-create_following_map = {item['Name']: item['Prompt'] for item in templates.get('You are creating the following...', [])}
-writing_style_map = {item['Name']: item['Prompt'] for item in templates.get('Writing Style', [])}
-goal_audience_map = {item['Name']: item['Prompt'] for item in templates.get('The goal for the audience is', [])}
-point_of_view_map = {item['Name']: item['Prompt'] for item in templates.get('Point of View', [])}
+expert_persona_details_map = {item['Name']: item['Prompt'] for item in templates.get('EXPERT & PERSONA DETAILS', [])}
+specific_instructions_map = {item['Name']: item['Prompt'] for item in templates.get('SPECIFIC INSTRUCTIONS', [])}
+writing_style_map = {item['Name']: item['Prompt'] for item in templates.get('WRITING STYLE', [])}
+goal_map = {item['Name']: item['Prompt'] for item in templates.get('GOAL', [])}
+audience_map = {item['Name']: item['Prompt'] for item in templates.get('AUDIENCE', [])}
 
 # Sidebar UI
 st.sidebar.title("Templates")
 num_instances = st.sidebar.number_input('Number of instances to create', min_value=1, max_value=10, value=1)
-selected_act_as = st.sidebar.selectbox("Act as if you are a...", [""] + list(act_as_map.keys()))
-selected_create_following = st.sidebar.selectbox("You are creating the following...", [""] + list(create_following_map.keys()))
-selected_style = st.sidebar.selectbox("Writing Style", [""] + list(writing_style_map.keys()))
-selected_goal_audience = st.sidebar.selectbox("The goal for the audience is", [""] + list(goal_audience_map.keys()))
-selected_point_of_view = st.sidebar.selectbox("Point of View", [""] + list(point_of_view_map.keys()))
+selected_expert_persona_details = st.sidebar.selectbox("EXPERT & PERSONA DETAILS", [""] + list(expert_persona_details_map.keys()))
+selected_specific_instructions = st.sidebar.selectbox("SPECIFIC INSTRUCTIONS", [""] + list(specific_instructions_map.keys()))
+selected_writing_style = st.sidebar.selectbox("WRITING STYLE", [""] + list(writing_style_map.keys()))
+selected_goal = st.sidebar.selectbox("GOAL", [""] + list(goal_map.keys()))
+selected_audience = st.sidebar.selectbox("AUDIENCE", [""] + list(audience_map.keys()))
 
 # Main UI
 st.title("Prompt Builder GUI")
 
 # Lists to hold input values
 company_names = []
-act_as_list = []
-create_following_list = []
+expert_persona_details_list = []
+specific_instructions_list = []
 writing_styles = []
-goal_audiences = []
-points_of_view = []
+goals = []
+audiences = []
 user_prompts = []
 
 # Collect inputs for each instance
 for i in range(num_instances):
     with st.expander(f'Instance {i + 1}', expanded=True):
         company_names.append(st.text_input(f"Company/Person Name {i + 1}"))
-        act_as_list.append(st.text_input(f"Act as if you are a... {i + 1}", value=act_as_map.get(selected_act_as, "")))
-        create_following_list.append(st.text_input(f"You are creating the following... {i + 1}", value=create_following_map.get(selected_create_following, "")))
-        writing_styles.append(st.text_input(f"Writing Style {i + 1}", value=writing_style_map.get(selected_style, "")))
-        goal_audiences.append(st.text_input(f"The goal for the audience is {i + 1}", value=goal_audience_map.get(selected_goal_audience, "")))
-        points_of_view.append(st.text_input(f"Point of View {i + 1}", value=point_of_view_map.get(selected_point_of_view, "")))
+        expert_persona_details_list.append(st.text_input(f"EXPERT & PERSONA DETAILS {i + 1}", value=expert_persona_details_map.get(selected_expert_persona_details, "")))
+        specific_instructions_list.append(st.text_input(f"SPECIFIC INSTRUCTIONS {i + 1}", value=specific_instructions_map.get(selected_specific_instructions, "")))
+        writing_styles.append(st.text_input(f"WRITING STYLE {i + 1}", value=writing_style_map.get(selected_writing_style, "")))
+        goals.append(st.text_input(f"GOAL {i + 1}", value=goal_map.get(selected_goal, "")))
+        audiences.append(st.text_input(f"AUDIENCE {i + 1}", value=audience_map.get(selected_audience, "")))
         user_prompts.append(st.text_input(f"User Prompt {i + 1}"))
 
 # Generate prompt action
 if st.button("Generate Prompt"):
     for i in range(num_instances):
-        original_text = '''Please analyze and strictly adhere to all of the the instructions in provided in this prompt. You will will follow these instructions and do your best fulfilling the following QUESTION here: [USER PROMPT] .
-In order to provide a perfect and complete output, access any and all information you have access, which includes: files and content stored in your database, attachments provided if any, as well as any content provided within this prompt. Output Instructions: 
-
-1. You are to act as if you are the person or representative described here when creating your output: [ACT AS IF YOU ARE A...]
-
-2. You are producing this content on behalf of: [Company/Person Name]
-
-3. You are tasked to create the following: [YOU ARE CREATING THE FOLLOWING...]
-
-4. You should write in the following point of view: [POINT OF VIEW]
-
-5. Your primary goal with this task is:  [THE GOAL FOR THE AUDIENCE IS]
-
-6. Strictly adhere to the following WRITING STYLE: [WRITING STYLE]
-
-Please provide your output for the QUESTION provided above while strictly adhering to the instructions contained within this prompt.'''
+        original_text = '''
+        You are [EXPERT & PERSONA DETAILS]. Your mission is detailed under ‘TASK’. Ensure that your output aligns with the ‘GOAL’ and is tailored for ‘AUDIENCE’. Use any resources provided, including the content of this prompt, attachments or your database. The final output will be sent directly to the user. Avoid placeholders or generic labels. Ensure your response matches the ‘WRITING STYLE’ provided.
+TASK: [SPECIFIC INSTRUCTIONS]
+AUDIENCE: [AUDIENCE]
+WRITING STYLE: [WRITING STYLE]
+GOAL: [GOAL]'''
         
         updated_text = original_text
         updated_text = updated_text.replace("[Company/Person Name]", company_names[i])
-        updated_text = updated_text.replace("[ACT AS IF YOU ARE A...]", act_as_list[i])
-        updated_text = updated_text.replace("[YOU ARE CREATING THE FOLLOWING...]", create_following_list[i])
+        updated_text = updated_text.replace("[EXPERT & PERSONA DETAILS]", expert_persona_details[i])
+        updated_text = updated_text.replace("[SPECIFIC INSTRUCTIONS]", specific_instructions_list[i])
+        updated_text = updated_text.replace("[AUDIENCE]", audiences[i])
         updated_text = updated_text.replace("[WRITING STYLE]", writing_styles[i])
-        updated_text = updated_text.replace("[THE GOAL FOR THE AUDIENCE IS]", goal_audiences[i])
-        updated_text = updated_text.replace("[POINT OF VIEW]", points_of_view[i])
+        updated_text = updated_text.replace("[GOAL]", goals[i])
         updated_text = updated_text.replace("[USER PROMPT]", user_prompts[i])
         
         st.subheader(f"Prompt for Instance {i + 1}")
