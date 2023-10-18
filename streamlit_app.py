4a033d3ef7ece1fdf42f74c2eb36d9c97a68befe
EthanDem <113375436+EthanDem@users.noreply.github.com>
2023-10-18 15:51:01 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,6 @@ with open('/app/prompt-builder/fixed_fully_updated_prompt_builder_templates.json
     templates = json.load(f)
 
 # Create maps for each template category
-expert_persona_details_map = {item['Name']: item['Prompt'] for item in templates.get('EXPERT & PERSONA DETAILS', [])}
 specific_instructions_map = {item['Name']: item['Prompt'] for item in templates.get('SPECIFIC INSTRUCTIONS', [])}
 writing_style_map = {item['Name']: item['Prompt'] for item in templates.get('WRITING STYLE', [])}
 goal_map = {item['Name']: item['Prompt'] for item in templates.get('GOAL', [])}
@@ -15,7 +14,6 @@ audience_map = {item['Name']: item['Prompt'] for item in templates.get('AUDIENCE
 # Sidebar UI
 st.sidebar.title("Templates")
 num_instances = st.sidebar.number_input('Number of instances to create', min_value=1, max_value=10, value=1)
-selected_expert_persona_details = st.sidebar.selectbox("EXPERT & PERSONA DETAILS", [""] + list(expert_persona_details_map.keys()))
 selected_specific_instructions = st.sidebar.selectbox("SPECIFIC INSTRUCTIONS", [""] + list(specific_instructions_map.keys()))
 selected_writing_style = st.sidebar.selectbox("WRITING STYLE", [""] + list(writing_style_map.keys()))
 selected_goal = st.sidebar.selectbox("GOAL", [""] + list(goal_map.keys()))
@@ -26,7 +24,6 @@ st.title("Prompt Builder GUI")
 
 # Lists to hold input values
 company_names = []
-expert_persona_details_list = []
 specific_instructions_list = []
 writing_styles = []
 goals = []
@@ -37,7 +34,6 @@ user_prompts = []
 for i in range(num_instances):
     with st.expander(f'Instance {i + 1}', expanded=True):
         company_names.append(st.text_input(f"Company/Person Name {i + 1}"))
-        expert_persona_details_list.append(st.text_input(f"EXPERT & PERSONA DETAILS {i + 1}", value=expert_persona_details_map.get(selected_expert_persona_details, "")))
         specific_instructions_list.append(st.text_input(f"SPECIFIC INSTRUCTIONS {i + 1}", value=specific_instructions_map.get(selected_specific_instructions, "")))
         writing_styles.append(st.text_input(f"WRITING STYLE {i + 1}", value=writing_style_map.get(selected_writing_style, "")))
         goals.append(st.text_input(f"GOAL {i + 1}", value=goal_map.get(selected_goal, "")))
@@ -48,7 +44,7 @@ for i in range(num_instances):
 if st.button("Generate Prompt"):
     for i in range(num_instances):
         original_text = '''
-        You are [EXPERT & PERSONA DETAILS]. Your mission is detailed under ‘TASK’. Ensure that your output aligns with the ‘GOAL’ and is tailored for ‘AUDIENCE’. Use any resources provided, including the content of this prompt, attachments or your database. The final output will be sent directly to the user. Avoid placeholders or generic labels. Ensure your response matches the ‘WRITING STYLE’ provided.
+        You are [Company/Person Name]. Your mission is detailed under ‘TASK’. Ensure that your output aligns with the ‘GOAL’ and is tailored for ‘AUDIENCE’. Use any resources provided, including the content of this prompt, attachments or your database. The final output will be sent directly to the user. Avoid placeholders or generic labels. Ensure your response matches the ‘WRITING STYLE’ provided.
 TASK: [SPECIFIC INSTRUCTIONS]
 AUDIENCE: [AUDIENCE]
 WRITING STYLE: [WRITING STYLE]
@@ -56,7 +52,6 @@ GOAL: [GOAL]'''
         
         updated_text = original_text
         updated_text = updated_text.replace("[Company/Person Name]", company_names[i])
-        updated_text = updated_text.replace("[EXPERT & PERSONA DETAILS]", expert_persona_details[i])
         updated_text = updated_text.replace("[SPECIFIC INSTRUCTIONS]", specific_instructions_list[i])
         updated_text = updated_text.replace("[AUDIENCE]", audiences[i])
         updated_text = updated_text.replace("[WRITING STYLE]", writing_styles[i])
